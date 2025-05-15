f0db15906191fb126520ad263bca507f1fcd3b68
jimmycaldwell <jlcaldwell132@gmail.com>
2025-05-15 17:18:03 -0400

    setting up terraform and build pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,307 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "my_vpc"
+  }
+}
+
+# Subnets (Public)
+resource "aws_subnet" "my_subnet_1" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "vm_subnet_public"
+    "kubernetes.io/role/elb"     = "1" #this instruct the kubernetes to create public load balancer in these subnets
+    "kubernetes.io/cluster/demo" = "owned"
+  }
+}
+# Subnet in us-east-1b (private)
+resource "aws_subnet" "subnet_us_east_1b_private" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = false  # Private subnet
+
+  tags = {
+    Name = "subnet_us_east_1b_private"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/demo"      = "owned"
+  }
+}
+# Subnet in us-east-1c (private)
+resource "aws_subnet" "subnet_us_east_1c_private" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = false  # Private subnet
+
+  tags = {
+    Name = "subnet_us_east_1b_private"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/demo"      = "owned"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "my_igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+}
+
+# routing table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+      cidr_block                 = "0.0.0.0/0"
+      nat_gateway_id             = aws_nat_gateway.k8s-nat.id
+    }
+
+  tags = {
+    Name = "private"
+  }
+}
+
+# Associate the route table with the subnet
+resource "aws_route_table_association" "my_subnet_association" {
+  subnet_id      = aws_subnet.my_subnet_1.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+resource "aws_route_table_association" "private-us-east-1b" {
+  subnet_id      = aws_subnet.subnet_us_east_1b_private.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private-us-east-1c" {
+  subnet_id      = aws_subnet.subnet_us_east_1c_private.id
+  route_table_id = aws_route_table.private.id
+}
+
+# Security Group for EC2 Instance
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2_sg"
+  description = "Security group for EC2 instance"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 27017
+    to_port   = 27017
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+   }
+}
+
+resource "aws_key_pair" "ssh_keypair" {
+  key_name   = "jimmyc-keypair"
+  public_key = file("~/.ssh/id_ed25519.pub")
+}
+# EC2 Instance
+resource "aws_instance" "my_instance" {
+  ami             = "ami-0fc5d935ebf8bc3bc" # Ubuntu 20.04 LTS
+  instance_type   = "t2.micro"
+  key_name        = aws_key_pair.ssh_keypair.key_name
+  subnet_id       = aws_subnet.my_subnet_1.id
+  security_groups  = [aws_security_group.ec2_sg.id]
+  associate_public_ip_address = true
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install gnupg curl
+              curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
+              gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+              --dearmor
+              echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+              apt-get update
+              apt-get install -y mongodb-org
+              systemctl start mongod
+              systemctl enable mongodb
+              EOF
+ tags = {
+    Name = "mg-db-server"
+
+ }
+}
+
+resource "aws_eip" "nat" {
+  vpc = true
+
+  tags = {
+    Name = "nat"
+  }
+}
+
+resource "aws_nat_gateway" "k8s-nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.my_subnet_1.id
+
+  tags = {
+    Name = "k8s-nat"
+  }
+
+  depends_on = [aws_internet_gateway.my_igw]
+}
+
+# IAM role for eks
+
+resource "aws_iam_role" "demo" {
+  name = "eks-cluster-demo"
+  tags = {
+    tag-key = "eks-cluster-demo"
+  }
+
+  assume_role_policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": [
+                    "eks.amazonaws.com"
+                ]
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+POLICY
+}
+
+# eks policy attachment
+
+resource "aws_iam_role_policy_attachment" "demo-AmazonEKSClusterPolicy" {
+  role       = aws_iam_role.demo.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# bare minimum requirement of eks
+
+resource "aws_eks_cluster" "demo" {
+  name     = "demo"
+  role_arn = aws_iam_role.demo.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.subnet_us_east_1b_private.id,
+      aws_subnet.subnet_us_east_1c_private.id,
+      aws_subnet.my_subnet_1.id
+    ]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.demo-AmazonEKSClusterPolicy]
+}
+
+# role for nodegroup
+
+resource "aws_iam_role" "nodes" {
+  name = "eks-node-group-nodes"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+# IAM policy attachment to nodegroup
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.nodes.name
+}
+
+
+# aws node group
+
+resource "aws_eks_node_group" "private-nodes" {
+  cluster_name    = aws_eks_cluster.demo.name
+  node_group_name = "private-nodes"
+  node_role_arn   = aws_iam_role.nodes.arn
+
+  subnet_ids = [
+    aws_subnet.subnet_us_east_1b_private.id,
+    aws_subnet.subnet_us_east_1c_private.id
+  ]
+
+  capacity_type  = "ON_DEMAND"
+  instance_types = ["t2.micro"]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  labels = {
+    node = "kubenode02"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.nodes-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.nodes-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
