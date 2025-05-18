8a5cda6c74d313371536d940883de5d81740c8cb
jimmycaldwell <jlcaldwell132@gmail.com>
2025-05-18 11:12:34 -0400

    fixing github action for deploying

--- terraform/main.tf
+++ terraform/main.tf
@@ -240,7 +240,7 @@ resource "aws_instance" "my_instance" {
   instance_type   = "t2.micro"
   key_name        = aws_key_pair.ssh_keypair.key_name
   subnet_id       = aws_subnet.my_subnet_1.id
-  security_groups  = [aws_security_group.ec2_sg.id]
+  vpc_security_group_ids  = [aws_security_group.ec2_sg.id]
   associate_public_ip_address = true
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
   user_data = <<-EOF
